da7a10615497974fb20a8a3ab936a2059b038df1
pablo <pablo.triplem@gmail.com>
2023-07-26 00:35:59 +0200

    Fix session bug

--- src/routers/user_service.py
+++ src/routers/user_service.py
@@ -1,18 +1,28 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Body, Depends, HTTPException, status
+from sqlmodel import Session
 
 from src.classes.mail import email_manager
 from src.classes.user_manager import UserManager
+from src.database import engine
 from src.models import UserBase, UserInput, UserRoles
 from src.utils.authorization import is_doctor_or_admin, is_admin
 
 router = APIRouter(prefix="/user", tags=["user"])
 
 
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
 @router.post("/send-activate-account")
 async def send_activate_account(
-    email: Annotated[str, Body(embed=True)], _=Depends(is_doctor_or_admin)
+    email: Annotated[str, Body(embed=True)],
+    _=Depends(is_doctor_or_admin),
+    *,
+    session: Session = Depends(get_session),
 ):
     """
     Send email with token to activate user account
@@ -27,14 +37,14 @@ async def send_activate_account(
     Nothing
     """
 
-    user = UserManager.create_user(email=email, password="temp")
+    user = UserManager.create_user(email=email, password="temp", session=session)
     if user:
         return await email_manager.send_activate_account(to=email, token=user.token)
     raise HTTPException(status_code=400, detail="Email already exists")
 
 
 @router.post("/activate")
-async def activate(data: UserInput):
+async def activate(data: UserInput, *, session: Session = Depends(get_session)):
     """
     Activate a user account with a token sent to email address when user was created
 
@@ -54,7 +64,7 @@ async def activate(data: UserInput):
         If token is invalid
 
     """
-    user = UserManager.activate_user_to_patient(data)
+    user = UserManager.activate_user_to_patient(data, session=session)
     if user:
         return user
     raise HTTPException(status_code=400, detail="Invalid token")
@@ -62,7 +72,11 @@ async def activate(data: UserInput):
 
 @router.post("/register", response_model=UserBase)
 async def register(
-    email: Annotated[str, Body(embed=True)], role: UserRoles, _=Depends(is_admin)
+    email: Annotated[str, Body(embed=True)],
+    role: UserRoles,
+    _=Depends(is_admin),
+    *,
+    session: Session = Depends(get_session),
 ):
     """
     Admin creates a new admin or doctor account
@@ -81,17 +95,23 @@ async def register(
 
     """
     try:
-        user = UserManager.create_user(email=email, password="temp")
+        user = UserManager.create_user(email=email, password="temp", session=session)
         if user:
             UserManager.set_user_role(user, role)
             await email_manager.send_activate_account(to=user.email, token=user.token)
         return user
-    except Exception:
-        raise HTTPException(status_code=400, detail="Error while creating user")
+    except Exception as e:
+        raise HTTPException(
+            status_code=400, detail=f"Error while creating user in register{e}"
+        )
 
 
 @router.post("/request-register", status_code=status.HTTP_201_CREATED)
-async def request_register(email: Annotated[str, Body(embed=True)], role: UserRoles):
+async def request_register(
+    email: Annotated[str, Body(embed=True)],
+    role: UserRoles,
+    session: Session = Depends(get_session),
+):
     """
     Request admin to create a new admin or doctor account
     It creates a new user with the status "pending"
@@ -107,16 +127,25 @@ async def request_register(email: Annotated[str, Body(embed=True)], role: UserRo
 
     """
     try:
-        user = UserManager.create_user(email=email, password="temp", status="pending")
+        user = UserManager.create_user(
+            email=email, password="temp", status="pending", session=session
+        )
         if user:
-            UserManager.set_user_role(user, role)
-            return {"message": "User created successfully"}
-    except Exception:
-        raise HTTPException(status_code=400, detail="Error while creating user")
+            try:
+                UserManager.set_user_role(user, role, session=session)
+                return {"message": "User created successfully"}
+            except Exception as e:
+                UserManager.delete_user(user, session=session)
+                raise e
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Error while creating user {e}")
 
 
 @router.post("/activate-pending-user", response_model=UserBase)
-async def activate_pending_user(email: str, _=Depends(is_admin)):
+async def activate_pending_user(
+    email: str, _=Depends(is_admin), *, session: Session = Depends(get_session)
+):
     """
     Activate a pending user account
     Parameters
@@ -130,7 +159,7 @@ async def activate_pending_user(email: str, _=Depends(is_admin)):
         User object if user was activated successfully
 
     """
-    user = UserManager.activate_pending_user(email)
+    user = UserManager.activate_pending_user(email, session=session)
     if user:
         return user
     raise HTTPException(
