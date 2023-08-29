b5b117b677846cb970b0fa77c4b2d9ec34eb7b92
pablo <pablo.triplem@gmail.com>
2023-08-30 01:50:04 +0200

    Refactor Questions logic getter

--- src/routers/module_service.py
+++ src/routers/module_service.py
@@ -1,10 +1,30 @@
 from fastapi import APIRouter, Depends
 
-from src.classes.modules_manager import ModuleNotFound, ModuleManager
+from src.classes.modules_manager import ModuleManager, ModuleNotFound
 from src.utils.authorization import is_doctor_or_admin
 from src.utils.reuse import get_session
 
 router = APIRouter(prefix="/module", tags=["Modulos"])
+
+
+@router.get("/")
+async def get_modules(
+    session=Depends(get_session), is_admin_or_doctor=Depends(is_doctor_or_admin)
+):
+    """
+    Get all-modules
+
+    Returns
+    -------
+    list[Module]
+        List of all modules
+    """
+    return ModuleManager.get_modules(session=session)
+
+
+@router.get("/{id_module}/questions")
+async def get_questions(id_module: int, session=Depends(get_session)):
+    return ModuleManager.get_module_with_questions(id_module, session=session)
 
 
 @router.get("/{id_module}")
@@ -31,23 +51,3 @@ async def get_module(id_module: int, session=Depends(get_session)):
     if module:
         return module
     raise ModuleNotFound()
-
-
-@router.get("/")
-async def get_modules(
-    session=Depends(get_session), is_admin_or_doctor=Depends(is_doctor_or_admin)
-):
-    """
-    Get all-modules
-
-    Returns
-    -------
-    list[Module]
-        List of all modules
-    """
-    return ModuleManager.get_modules(session=session)
-
-
-@router.get("/{id_module}/questions")
-async def get_module_with_questions(id_module: str, session=Depends(get_session)):
-    return ModuleManager.get_module_with_questions(id_module, session=session)
